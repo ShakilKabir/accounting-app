622aaa7b5edd9228e92ceb9969ebe6dc30ff6a5b
shakilkabir <shaakilkabir@gmail.com>
2023-10-30 19:13:17 +0600

    Income Statement done

--- server/src/models/chartOfAccounts.ts
+++ server/src/models/chartOfAccounts.ts
@@ -10,7 +10,7 @@ const chartOfAccountsSchema = new mongoose.Schema({
     enum: ['Asset', 'Liability', 'Equity', 'Revenue', 'Expense'],
     required: true
   },
-  subtype: { type: String },  // Add validation or enums as needed
+  subtype: { type: String },
   description: { type: String },
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
@@ -24,7 +24,7 @@ export const initializeDefaultAccounts = async (userId: string) => {
       account_name: 'Cash',
       account_type: 'Asset',
       subtype: 'Current Asset',
-      description: 'Cash on hand',
+      description: 'Cash on hand and in bank',
     },
     {
       user_id: userId,
@@ -35,6 +35,41 @@ export const initializeDefaultAccounts = async (userId: string) => {
     },
     {
       user_id: userId,
+      account_name: 'Inventory',
+      account_type: 'Asset',
+      subtype: 'Current Asset',
+      description: 'Goods available for sale to customers',
+    },
+    {
+      user_id: userId,
+      account_name: 'Prepaid Expenses',
+      account_type: 'Asset',
+      subtype: 'Current Asset',
+      description: 'Expenses paid in advance such as insurance',
+    },
+    {
+      user_id: userId,
+      account_name: 'Equipment',
+      account_type: 'Asset',
+      subtype: 'Non-Current Asset',
+      description: 'Long-term assets such as machinery and equipment',
+    },
+    {
+      user_id: userId,
+      account_name: 'Building',
+      account_type: 'Asset',
+      subtype: 'Non-Current Asset',
+      description: 'Real estate owned by the company',
+    },
+    {
+      user_id: userId,
+      account_name: 'Land',
+      account_type: 'Asset',
+      subtype: 'Non-Current Asset',
+      description: 'Cost of land owned by the company',
+    },
+    {
+      user_id: userId,
       account_name: 'Accounts Payable',
       account_type: 'Liability',
       subtype: 'Current Liability',
@@ -42,23 +77,87 @@ export const initializeDefaultAccounts = async (userId: string) => {
     },
     {
       user_id: userId,
+      account_name: 'Accrued Expenses',
+      account_type: 'Liability',
+      subtype: 'Current Liability',
+      description: 'Expenses that have been incurred but not yet paid',
+    },
+    {
+      user_id: userId,
+      account_name: 'Unearned Revenue',
+      account_type: 'Liability',
+      subtype: 'Current Liability',
+      description: 'Payments received from customers for services yet to be delivered',
+    },
+    {
+      user_id: userId,
+      account_name: 'Notes Payable',
+      account_type: 'Liability',
+      subtype: 'Long-Term Liability',
+      description: 'Amounts owed that are due more than one year in the future',
+    },
+    {
+      user_id: userId,
+      account_name: 'Mortgage Payable',
+      account_type: 'Liability',
+      subtype: 'Long-Term Liability',
+      description: 'Long-term loans taken out on property',
+    },
+    {
+      user_id: userId,
       account_name: 'Sales Revenue',
       account_type: 'Revenue',
-      description: 'Income from sales',
+      description: 'Income from goods sold or services rendered',
+    },
+    {
+      user_id: userId,
+      account_name: 'Service Revenue',
+      account_type: 'Revenue',
+      description: 'Income from services provided to customers',
+    },
+    {
+      user_id: userId,
+      account_name: 'Interest Revenue',
+      account_type: 'Revenue',
+      description: 'Income earned from investments and interest-bearing accounts',
     },
     {
       user_id: userId,
       account_name: 'Rent Expense',
       account_type: 'Expense',
-      description: 'Expenses related to rented properties',
+      description: 'Periodic payments for rental of property or equipment',
     },
     {
       user_id: userId,
       account_name: 'Utilities Expense',
       account_type: 'Expense',
-      description: 'Expenses for utilities like electricity, water, etc.',
+      description: 'Costs of utilities such as electricity, water, and internet',
     },
-    // ... Add more predefined accounts as needed
+    {
+      user_id: userId,
+      account_name: 'Salaries Expense',
+      account_type: 'Expense',
+      description: 'Wages paid to employees',
+    },
+    {
+      user_id: userId,
+      account_name: 'Depreciation Expense',
+      account_type: 'Expense',
+      description: 'Allocation of the cost of tangible assets over their useful lives',
+    },
+    {
+      user_id: userId,
+      account_name: 'Owner’s Capital',
+      account_type: 'Equity',
+      description: 'Investment of cash or other assets by the owner into the business',
+    },
+    {
+      user_id: userId,
+      account_name: 'Retained Earnings',
+      account_type: 'Equity',
+      description: 'Earnings not distributed to shareholders and reinvested in the business',
+    },
+    // ... You can add or modify accounts as needed to fit the business context
   ];
 
   return ChartOfAccounts.insertMany(predefinedAccounts);
--- server/src/routes/transactionRoutes.ts
+++ server/src/routes/transactionRoutes.ts
@@ -20,20 +20,19 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  console.log('Processing POST request for /', req.userId);
+
   try {
       const { date, description, account, category, amount } = req.body;
       const { userId } = req;
       
       // Fetch the ObjectIds for account and category
-      const accountDocument = await ChartOfAccounts.findOne({ account_name: account });
-      const categoryDocument = await ChartOfAccounts.findOne({ account_name: category });
+      const accountDocument = await ChartOfAccounts.findOne({ account_name: account,user_id:userId });
+      const categoryDocument = await ChartOfAccounts.findOne({ account_name: category, user_id: userId });
 
       // Validate if we found the account and category
       if (!accountDocument || !categoryDocument) {
           return res.status(400).send({ message: 'Account or Category not found' });
       }
-
       const transactionData = {
           date,
           description,
@@ -71,5 +70,39 @@ router.delete('/:transaction_id', async (req, res) => {
     res.status(400).send(err);
   }
 });
+
+router.get('/income-statement', async (req, res) => {
+  console.log('Processing GET request for /', req.userId);
+  try {
+      const userId = req.userId;
+      console.log('userId', userId)
+
+      // Fetch account entries for Revenue and Expense types
+      const revenueAccounts = await ChartOfAccounts.find({ account_type: 'Revenue', user_id: userId });
+      const expenseAccounts = await ChartOfAccounts.find({ account_type: 'Expense', user_id: userId });
+
+      const revenueAccountIds = revenueAccounts.map(account => account._id);
+      const expenseAccountIds = expenseAccounts.map(account => account._id);
+
+      // Fetching all Revenue and Expense transactions for the user
+      const revenueTransactions = await Transaction.find({
+          user_id: userId,
+          $or: [
+              { category_type: { $in: revenueAccountIds } }
+          ]
+      });
+      const expenseTransactions = await Transaction.find({
+          user_id: userId,
+          $or: [
+              { category_type: { $in: expenseAccountIds } }
+          ]
+      });
+      res.status(200).send([revenueTransactions, expenseTransactions]);
+  } catch (err) {
+      console.log(err);
+      res.status(400).send(err);
+  }
+});
+
 
 export default router;
